62b2bbadcb730747f8f1f4f87670aece8f69b427
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-01-21 11:28:00 -0600

    add basic water line

---
+++ water.gd
@@ -0,0 +1,41 @@
+extends Node2D
+class_name Water
+
+
+@export var start_point: Node2D
+
+var points: PackedVector2Array
+
+
+func _ready() -> void:
+	update_water_line()
+	var line = Line2D.new()
+	line.default_color = Color.BLUE
+	line.width = 10
+	line.points = points
+	add_child(line)
+	
+
+
+func update_water_line():
+	points = [start_point.position]
+	
+	if not get_collisions_below():
+		points.append(get_bottom_point())
+
+
+func get_collisions_below() -> Dictionary:
+	var screen_height = get_viewport_rect().size.y
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(
+		start_point.position, 
+		Vector2(start_point.position.x, screen_height)
+	)
+	
+	return space_state.intersect_ray(query)
+
+
+func get_bottom_point() -> Vector2:
+	var screen_height = get_viewport_rect().size.y
+	return Vector2(points[-1].x, screen_height)
+	
