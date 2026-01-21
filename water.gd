0cb0b5f1fba9b151e3be513a2fdd086d8b559c16
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-01-21 11:54:58 -0600

    feature: stop water on tilemap

--- water.gd
+++ water.gd
@@ -6,9 +6,11 @@ class_name Water
 
 var points: PackedVector2Array
 var line: Line2D = Line2D.new()
+var stopped = false
+var collision_layers = 1
 
 
-func _ready() -> void:
+func _process(delta: float) -> void:
 	update_water_line()
 	draw_water_line()
 
@@ -23,24 +25,46 @@ func draw_water_line() -> void:
 
 
 func update_water_line() -> void:
+	stopped = false
 	points = [start_point.position]
 	
-	if not get_collisions_below():
+	add_point_below()
+
+
+func add_point_below() -> void:
+	var collision = get_collisions_below()
+	
+	if not collision:
 		points.append(get_bottom_point())
+		return
+		
+	points.append(collision["position"])
 
 
 func get_collisions_below() -> Dictionary:
+	var result = {}
+
 	var screen_height = get_viewport_rect().size.y
+	var bottom_point = Vector2(points[-1].x, screen_height)
+	result = get_collisions(bottom_point)
+	
+	return result
+
+
+func get_collisions(point: Vector2) -> Dictionary:
+	var result = {}
+	
 	var space_state = get_world_2d().direct_space_state
 	var query = PhysicsRayQueryParameters2D.create(
-		start_point.position, 
-		Vector2(start_point.position.x, screen_height)
+		points[-1], 
+		point,
+		collision_layers,
 	)
 	
-	return space_state.intersect_ray(query)
+	result = space_state.intersect_ray(query)
+	return result
 
 
 func get_bottom_point() -> Vector2:
 	var screen_height = get_viewport_rect().size.y
 	return Vector2(points[-1].x, screen_height)
-	
