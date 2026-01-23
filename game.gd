753c9887892772ba1f9fdd4c25536c049e98d9fd
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-01-23 11:57:20 -0600

    feature: add debug draw line, make Game a singleton

--- game.gd
+++ game.gd
@@ -9,3 +9,20 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func debug_draw_line(start_point: Vector2, end_point: Vector2, duration := 10.00, color := Color.MAGENTA) -> void:
+	var debug_line := Line2D.new()
+	debug_line.color = color
+	debug_line.add_point(start_point)
+	debug_line.add_point(end_point)
+	debug_line.z_index = 500
+	
+	var timer := Timer.new()
+	timer.timeout.connect(func():
+		debug_line.queue_free()
+	)
+	
+	add_child(debug_line)
+	debug_line.add_child(timer)
+	timer.start(duration)
