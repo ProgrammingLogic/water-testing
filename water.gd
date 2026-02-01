685e13b506272b01967611c625ec34b18c288b20
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-02-01 13:43:36 -0600

    feature: implement collision checking for water flow

--- water.gd
+++ water.gd
@@ -15,20 +15,21 @@ var _water_lines: Array[WaterLine] = []
 
 func _ready() -> void:
 	tile_size = tile_map.tile_set.tile_size
-	add_water_line([
-		Vector2.ZERO, 
-		Vector2(0, 100)
-	])
+	add_water_line([Vector2.ZERO])
 
 
 ## Add a water line to the water.
 ##
 ## Input:
-## - points: PackedVector2Array -> The points the water line is going to start 
+## - points: PackedVector2Array -> The points the water line is going to start
 ## 	with.
-func add_water_line(points: PackedVector2Array) -> void:
+## - flow_direction: Vector2i -> The direction the water is flowing.
+##
+## Output:
+## - None
+func add_water_line(points: PackedVector2Array, flow_direction := Vector2i.DOWN) -> void:
 	assert(not points.is_empty())
-	var water_line = WaterLine.new(self, tile_map, points)
+	var water_line = WaterLine.new(self, tile_map, points, flow_direction)
 
 	_water_lines.append(water_line)
 	add_child(water_line)
@@ -43,40 +44,120 @@ func add_water_line(points: PackedVector2Array) -> void:
 ## - has_point: bool -> Whether or not this water object has the point in any
 ##	of it's water lines.
 func has_point(query_point: Vector2) -> bool:
-	var query_cell = translate_point_to_tilemap(query_point)
+	var global_query_point = to_global(query_point)
+
+	var local_tile_map_query_point := tile_map.to_local(global_query_point)
+	var tile_map_cords_query_point := tile_map.local_to_map(
+		local_tile_map_query_point
+	)
 
 	for water_line in _water_lines:
 		for point in water_line.points:
-			var cell = translate_point_to_tilemap(point)
-			print("calculations:")
-			print("\tquery_point: %v, point: %v" % [query_point, point])
-			print("\tquery_cell %v, cell %v" % [query_cell, cell])
-	
+			var global_point := to_global(point)
+
+			var local_tile_map_point := tile_map.to_local(global_point)
+			var tile_map_cords_point = tile_map.local_to_map(local_tile_map_point)
+
+			print("query:")
+			print("\tglobal_query_point: %v, global_point: %v" % [global_query_point, global_point])
+			print("\ttile_map_cords_query_point: %v, tile_map_cords_point: %v"
+				% [tile_map_cords_query_point, tile_map_cords_point])
+
+			if tile_map_cords_query_point == tile_map_cords_point:
+				return true
+
 	return false
 
 
-## Translate a point to it's cell coordinates in the tile_map.
+## Translate a local point by a number of tiles equal to direction.
+##
+## This function takes the local point, translates it to water's position,
+##	and then determines moves that point towards direction, where the distance
+## 	it's translated is equal to tile_size * direction.
 ##
 ## Input:
-## - point: Vector2 -> The point to translate to cell coordinates.
-## 
+## - point: Vector2 -> The point to translate.
+## - direction: Vector2i -> How many tiles to move in each direction.
+##
 ## Output:
-## - cell_coordinates: Vector2i -> The coordinates the point aligns to on the
-##	tile map.
-func translate_point_to_tilemap(point: Vector2) -> Vector2i:
-	var cell_coordinates: Vector2i = floor(point / tile_size)
-	assert(cell_coordinates.x >= 0)
-	assert(cell_coordinates.y >= 0)
-	return cell_coordinates
+## - translated_point: Vector2 -> The local point translated by direction tiles.
+func translate_point_to_direction(point: Vector2, direction: Vector2i) -> Vector2:
+	var global_point = to_global(point)
+
+	var translation = direction as Vector2 * tile_size
+
+	var translated_global_point = global_point + translation
+	var translated_point = to_local(translated_global_point)
+
+	return translated_point
 
 
-## Check if a point is inside the water's viewport.
+## Translate a local water point to a position centered to the tile map.
+##
+## Input:
+## - point: Vector2 -> The point to center.
+##
+## Output:
+## - centered_point_global -> The point's global position centered on a tile.
+func translate_point_to_tile_center(point: Vector2) -> Vector2:
+	var global_point := to_global(point)
+
+	var local_tile_map_point := tile_map.to_local(global_point)
+	var tile_map_cords = tile_map.local_to_map(local_tile_map_point)
+
+	var centered_local_tile_map_point = tile_map.map_to_local(tile_map_cords)
+	var centered_global_point = tile_map.to_global(centered_local_tile_map_point)
+
+	var centered_local_point = to_local(centered_global_point)
+
+	return centered_local_point
+
+
+## Checks if the local point is valid.
 ##
 ## Input:
 ## - point: Vector2 -> The point to check.
 ##
 ## Output:
+## - result: bool -> Whether or not the point is valid.
+func is_point_valid(point: Vector2) -> bool:
+	if not is_inside_viewport(point):
+		return false
+
+	return true
+
+
+## Check if a local point is inside the viewport.
+##
+## Input:
+## - point: Vector2 -> The local point to check.
+##
+## Output:
 ## - inside_viewport: bool -> Whether or not the point is inside the viewport.
 func is_inside_viewport(point: Vector2) -> bool:
+	var global_point = to_global(point)
 	var viewport_rect = get_viewport_rect()
-	return viewport_rect.has_point(point)
+	return viewport_rect.has_point(global_point)
+
+
+## Checks whether or not the local point is colliding with something.
+##
+## Uses physics layer 1 for collisions.
+##
+## Input:
+## - point: Vector2 -> The local point to check for collisions at.
+##
+## Output:
+## - result: bool -> Whether or not the point is colliding with something.
+func is_colliding(point: Vector2) -> bool:
+	var global_point = to_global(point)
+	assert(is_point_valid(global_point))
+
+	var space_state = get_world_2d().direct_space_state
+	assert(space_state != null)
+
+	var ray_query_parameters = PhysicsPointQueryParameters2D.new()
+	ray_query_parameters.position = global_point
+
+	var collisions := space_state.intersect_point(ray_query_parameters)
+	return collisions.is_empty()
--- water_line.gd
+++ water_line.gd
@@ -7,28 +7,141 @@ class_name WaterLine
 ## - tile_map: TileMapLayer -> The tilemap this water line is drawn on.
 ## - starting_points: PackedVector2Array -> The points this water line starts
 ##	on.
+## - flow_direction: Vector2i -> The direction the water is flowing.
+##	Defaults to Vector2i.DOWN.
 
 var _tile_map: TileMapLayer
-var _tile_size: Vector2
+var _tile_size: Vector2i
 var _water: Water
 var _starting_points: PackedVector2Array
+var _flow_direction: Vector2i
 
 
-func _init(water: Water, tile_map: TileMapLayer, starting_points: PackedVector2Array) -> void:
+func _init(water: Water, tile_map: TileMapLayer, starting_points: PackedVector2Array, flow_direction := Vector2i.DOWN) -> void:
 	_water = water
 	_tile_map = tile_map
 	_starting_points = starting_points
 	_tile_size = _tile_map.tile_set.tile_size
-	
+	_flow_direction = flow_direction
+
 	default_color = Color.BLUE
 	width = _tile_size.x / 4
 
 
 func _ready() -> void:
 	assert(not _starting_points.is_empty())
-	
+	assert(not _tile_size == null)
+	assert(not _tile_size == Vector2i.ZERO)
+
 	for point in _starting_points:
 		add_water_point(point)
+
+	flow()
+	assert(points.size() > _starting_points.size())
+
+
+## Calculates the water points for this water line, and then adds them to the
+##	water line.
+##
+## Input:
+## - None
+##
+## Output:
+## - None
+func flow() -> void:
+	assert(not _starting_points.is_empty())
+	assert(not points.size() > _starting_points.size())
+
+	while true:
+		var next_water_points := get_next_water_points()
+		var is_empty = next_water_points.is_empty()
+
+		if next_water_points.is_empty():
+			return
+
+		var next_point = next_water_points.pop_front()
+		var next_point_direction = next_point["direction"]
+		var next_point_vector = next_point["point"]
+		_flow_direction = next_point_direction
+		add_water_point(next_point_vector)
+
+		if next_water_points.is_empty():
+			return
+
+		while not next_water_points.is_empty():
+			next_point = next_water_points.pop_front()
+			next_point_direction = next_point["direction"]
+			next_point_vector = next_point["point"]
+			_water.add_water_line([next_point_vector], next_point_direction)
+
+	return
+
+
+## Calculate the next water points and their direction from the current point.
+##
+## Using a set of rules, this function will determine the next points in the
+##	water line, and the direction those points are from the current point.
+##
+## If there are multiple cells returned, it is assumed the caller of this
+##	function will determine what to do with the additional cells.
+##
+## Input:
+## - None
+##
+## Output:
+## - next_water_points: Array[Dictionary]
+## 		[{
+##			point: Vector2,
+##			direction: Vector2i
+##		}]
+func get_next_water_points() -> Array[Dictionary]:
+	var next_water_points: Array[Dictionary] = []
+
+	assert(not points.is_empty())
+	var current_water_point = points[-1]
+	assert(_water.is_point_valid(current_water_point))
+
+	var point_below = _water.translate_point_to_direction(current_water_point, Vector2i.DOWN)
+	var point_left = _water.translate_point_to_direction(current_water_point, Vector2i.LEFT)
+	var point_right = _water.translate_point_to_direction(current_water_point, Vector2i.RIGHT)
+
+	if can_flow_to(point_below):
+		next_water_points.append({
+			"point": point_below,
+			"direction": Vector2i.DOWN,
+		})
+		return next_water_points
+
+	if can_flow_to(point_left):
+		next_water_points.append({
+			"point": point_left,
+			"direction": Vector2i.LEFT,
+		})
+
+	if can_flow_to(point_right):
+		next_water_points.append({
+			"point": point_right,
+			"direction": Vector2i.RIGHT,
+		})
+
+	return next_water_points
+
+
+## Checks if the water line can flow to the point.
+##
+## Input:
+## - point: Vector2 -> The point to check.
+##
+## Output:
+## - result: bool -> Whether or not the water line can flow to point.
+func can_flow_to(point: Vector2) -> bool:
+	if not _water.is_point_valid(point):
+		return false
+
+	if not _water.is_colliding(point):
+		return false
+
+	return true
 
 
 ## Add a water point to the WaterLine's line.
@@ -42,8 +155,8 @@ func _ready() -> void:
 ## Output:
 ## - None
 func add_water_point(point: Vector2) -> void:
-	assert(_water.is_inside_viewport(point))
-	
+	assert(_water.is_point_valid(point))
+
 	# It's okay if another water line already has our starting points, because
 	#	there are circumstances where a water line will "split off" from
 	#	another water line, and they need to be able to connect.
