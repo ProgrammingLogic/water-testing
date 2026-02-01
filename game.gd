e800bd45a04d84703c7afb075de568964a7e7433
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-02-01 09:43:00 -0600

    cleanup: remove unneeded mouse input

--- game.gd
+++ game.gd
@@ -9,11 +9,6 @@ func _ready() -> void:
 	pass
 
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_released("mouse_click"):
-		get_viewport().get_global_mouse_position()
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
