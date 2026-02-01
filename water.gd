681d8e3797cfacf1f482bc4ca8b068bf834a1966
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-02-01 10:28:34 -0600

    feature: draw water line to screen

--- water.gd
+++ water.gd
@@ -1,13 +1,82 @@
 extends Node2D
 class_name Water
+## A water object.
+##
+## This water object has a collection of water_lines (streams) that it manages.
+##	These water lines start at water's position and flow from there.
 
 
 @export var tile_map: TileMapLayer
 
+var tile_size: Vector2
 
 var _water_lines: Array[WaterLine] = []
 
 
 func _ready() -> void:
-	await tile_map.ready
-	_water_lines.append(WaterLine.new(self, tile_map))
+	tile_size = tile_map.tile_set.tile_size
+	add_water_line([
+		Vector2.ZERO, 
+		Vector2(0, 100)
+	])
+
+
+## Add a water line to the water.
+##
+## Input:
+## - points: PackedVector2Array -> The points the water line is going to start 
+## 	with.
+func add_water_line(points: PackedVector2Array) -> void:
+	assert(not points.is_empty())
+	var water_line = WaterLine.new(self, tile_map, points)
+
+	_water_lines.append(water_line)
+	add_child(water_line)
+
+
+## Check if this Water element has the specific point.
+##
+## Input:
+## - query_point: Vector2 -> The point we are querying.
+##
+## Output
+## - has_point: bool -> Whether or not this water object has the point in any
+##	of it's water lines.
+func has_point(query_point: Vector2) -> bool:
+	var query_cell = translate_point_to_tilemap(query_point)
+
+	for water_line in _water_lines:
+		for point in water_line.points:
+			var cell = translate_point_to_tilemap(point)
+			print("calculations:")
+			print("\tquery_point: %v, point: %v" % [query_point, point])
+			print("\tquery_cell %v, cell %v" % [query_cell, cell])
+	
+	return false
+
+
+## Translate a point to it's cell coordinates in the tile_map.
+##
+## Input:
+## - point: Vector2 -> The point to translate to cell coordinates.
+## 
+## Output:
+## - cell_coordinates: Vector2i -> The coordinates the point aligns to on the
+##	tile map.
+func translate_point_to_tilemap(point: Vector2) -> Vector2i:
+	var cell_coordinates: Vector2i = floor(point / tile_size)
+	assert(cell_coordinates.x >= 0)
+	assert(cell_coordinates.y >= 0)
+	return cell_coordinates
+
+
+## Check if a point is inside the water's viewport.
+##
+## Input:
+## - point: Vector2 -> The point to check.
+##
+## Output:
+## - inside_viewport: bool -> Whether or not the point is inside the viewport.
+func is_inside_viewport(point: Vector2) -> bool:
+	var viewport_rect = get_viewport_rect()
+	return viewport_rect.has_point(point)
--- water_line.gd
+++ water_line.gd
@@ -1,12 +1,53 @@
 extends Line2D
 class_name WaterLine
-
 ## Line representing a water stream in a Water object.
+##
+## Input:
+## - water: Water -> The water object this water_line is a part of.
+## - tile_map: TileMapLayer -> The tilemap this water line is drawn on.
+## - starting_points: PackedVector2Array -> The points this water line starts
+##	on.
 
 var _tile_map: TileMapLayer
+var _tile_size: Vector2
 var _water: Water
+var _starting_points: PackedVector2Array
 
 
-func _init(water: Water, tile_map: TileMapLayer) -> void:
+func _init(water: Water, tile_map: TileMapLayer, starting_points: PackedVector2Array) -> void:
 	_water = water
 	_tile_map = tile_map
+	_starting_points = starting_points
+	_tile_size = _tile_map.tile_set.tile_size
+	
+	default_color = Color.BLUE
+	width = _tile_size.x / 4
+
+
+func _ready() -> void:
+	assert(not _starting_points.is_empty())
+	
+	for point in _starting_points:
+		add_water_point(point)
+
+
+## Add a water point to the WaterLine's line.
+##
+## Determines where in a cell the point should be added (center, right, left,
+## 	bottom center), and then adds a point at that position to the WaterLine.
+##
+## Input:
+## - point: Vector2 -> The position of the cell we want to add a point to.
+##
+## Output:
+## - None
+func add_water_point(point: Vector2) -> void:
+	assert(_water.is_inside_viewport(point))
+	
+	# It's okay if another water line already has our starting points, because
+	#	there are circumstances where a water line will "split off" from
+	#	another water line, and they need to be able to connect.
+	if not _starting_points.has(point):
+		assert(not _water.has_point(point))
+
+	add_point(point)
